315bef24eb852ac934a7731a4c5fc3e833b72359
Nexustix <nexus1768@tuta.io>
2016-07-12 23:48:01 +0200

    Add  comments to "gulpfile.js"

--- gulpfile.js
+++ gulpfile.js
@@ -6,23 +6,26 @@ var shell = require('gulp-shell')
 var credentials = require('./secrets.js');
 
 gulp.task('default', function() {
-  // place code for your default task here
 });
 
+//HACK updates submodules
 gulp.task('updatedeps', shell.task([
   'rm -R ./lib/HaxeScreeps',
   'git submodule update --remote',
 ]))
 
+//compiles project to ./dist folder
 gulp.task('compile', shell.task([
   'haxe build.hxml'
 ]))
 
+//deploys project to "test" branch
 gulp.task('test', ['compile'], function() {
   gulp.src('dist/*.js')
     .pipe(screeps(credentials));
 });
 
+//deploys project to "default" branch (untested)
 gulp.task('deploy', ['compile'], function() {
     credentials.branch = "default";
 
